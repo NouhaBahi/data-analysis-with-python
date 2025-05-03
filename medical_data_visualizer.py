06bc1a125356481f3e5da35a10c6b9e014faab99
NouhaBahi <nouhabahi1@gmail.com>
2025-05-03 23:16:46 +0100

    Add files via upload

---
+++ medical_data_visualizer.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 1 - Import data
+df = pd.read_csv('medical_examination.csv')
+
+# 2 - Add overweight column
+df['overweight'] = ((df['weight'] / ((df['height'] / 100) ** 2)) > 25).astype(int)
+
+# 3 - Normalize cholesterol and gluc
+df['cholesterol'] = (df['cholesterol'] > 1).astype(int)
+df['gluc'] = (df['gluc'] > 1).astype(int)
+
+# 4
+def draw_cat_plot():
+    # 5 - Create DataFrame for cat plot
+    df_cat = pd.melt(df, id_vars=['cardio'], 
+                     value_vars=['cholesterol', 'gluc', 'smoke', 
+                                'alco', 'active', 'overweight'])
+
+    # 6 - Group and reformat data
+    df_cat = pd.DataFrame(df_cat.groupby(['cardio', 'variable', 'value'])['value'].count())
+    df_cat.rename(columns={'value': 'total'}, inplace=True)
+    df_cat.reset_index(inplace=True)
+
+    # 7 - Create catplot
+    fig = sns.catplot(data=df_cat, x='variable', y='total',
+                      hue='value', col='cardio', kind='bar').fig
+
+    # 8 - Get the figure for the output
+    return fig
+
+def draw_heat_map():
+    # 11 - Clean the data
+    df_heat = df[
+        (df['ap_lo'] <= df['ap_hi']) & 
+        (df['height'] >= df['height'].quantile(0.025)) &
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) &
+        (df['weight'] <= df['weight'].quantile(0.975))
+    ]
+
+    # 12 - Calculate correlation matrix
+    corr = df_heat.corr()
+
+    # 13 - Generate mask for upper triangle
+    mask = np.triu(np.ones_like(corr))
+
+    # 14 - Set up the matplotlib figure
+    fig, ax = plt.subplots(figsize=(11, 9))
+
+    # 15 - Create heatmap
+    sns.heatmap(corr, mask=mask, annot=True, fmt='.1f', 
+                center=0, square=True, linewidths=.5)
+
+    # 16
+    fig.savefig('heatmap.png')
+    return fig
